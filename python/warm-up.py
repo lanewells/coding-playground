d1de9dd378ae268fe41eb6a47fecb38ae9daa029
lanewells <180503664+lanewells@users.noreply.github.com>
2026-02-28 17:09:08 -0800

    py exercise 3

--- python/warm-up.py
+++ python/warm-up.py
@@ -25,10 +25,37 @@ import datetime
 
 # Solution:
 
-num = input("Give me a number: ")
-remainder = int(num) % 2
-if remainder == 1:
-    result = 'odd'
-elif remainder == 0:
-    result = 'even'
-print('Your number (' + str(num) + ') is an ' + result + ' number.')
+# num = input("Give me a number: ")
+# remainder = int(num) % 2
+# if remainder == 1:
+#     result = 'odd'
+# elif remainder == 0:
+#     result = 'even'
+# print('Your number (' + str(num) + ') is an ' + result + ' number.')
+
+
+# Exercise 3: List less than ten
+
+# Take a list, say for example this one:
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+# and write a program that prints out all the elements of the list that are less than 5.
+
+# Solution:
+
+# basic
+for x in a:
+    if x < 5:
+        print(x)
+
+# return in new list
+new_list = []
+
+for x in a:
+    if x < 5:
+        new_list.append(x)
+print(new_list)
+
+# one line code
+print([x for x in a if x < 5])
+
