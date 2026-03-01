d067e9a345367cc862fe541448899678b98f76d5
lanewells <180503664+lanewells@users.noreply.github.com>
2026-02-28 17:46:23 -0800

    py exercise 4

--- python/exercises.py
+++ python/exercises.py
@@ -17,6 +17,7 @@ import datetime
 # => Lane, in the year 2097 you will be 100 years old.
 
 
+
 # Exercise 2: Odd or even
 
 # Ask the user for a number. Depending on whether the number is even or odd, 
@@ -34,28 +35,48 @@ import datetime
 # print('Your number (' + str(num) + ') is an ' + result + ' number.')
 
 
+
 # Exercise 3: List less than ten
 
 # Take a list, say for example this one:
 
-a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+# a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 # and write a program that prints out all the elements of the list that are less than 5.
 
 # Solution:
 
-# basic
-for x in a:
-    if x < 5:
-        print(x)
+# # basic
+# for x in a:
+#     if x < 5:
+#         print(x)
 
-# return in new list
-new_list = []
+# # return in new list
+# new_list = []
 
-for x in a:
-    if x < 5:
-        new_list.append(x)
+# for x in a:
+#     if x < 5:
+#         new_list.append(x)
+# print(new_list)
+
+# # one line code
+# print([x for x in a if x < 5])
+
+
+
+# Exercise 4: Divisors
+
+# Create a program that asks the user for a number and then prints out a list of all the divisors of that number. 
+# (If you don’t know what a divisor is, it is a number that divides evenly into another number. For example, 
+# 13 is a divisor of 26 because 26 / 13 has no remainder.)
+
+# Solution: 
+
+num = int(input("Give me a number: "))
+a = range(1, num)
+new_list = [x for x in a if num % x == 0]
 print(new_list)
 
-# one line code
-print([x for x in a if x < 5])
+# refactored and accounting for number itself as a divisor:
+num = int(input("Give me a number: "))
+new_list = print([x for x in range(1, num + 1) if num % x == 0])
 
