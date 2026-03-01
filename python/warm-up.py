e483dfc94d594330ef523595539efa074eeb8168
lanewells <180503664+lanewells@users.noreply.github.com>
2026-02-28 16:46:20 -0800

    py exercise 1

---
+++ python/warm-up.py
@@ -0,0 +1,19 @@
+# Exercise 1: Character input
+
+# Create a program that asks the user to enter their name and their age. 
+# Print out a message addressed to them that tells them the year that 
+# they will turn 100 years old.
+
+# Solution:
+
+import datetime
+
+# name = input("What is your name?")
+# age = int(input("How old are you?"))
+# current_year = datetime.datetime.now().year
+# year = (100 - age) + current_year
+
+# print(f'{name}, in the year {year} you will be 100 years old.')
+# => Lane, in the year 2097 you will be 100 years old.
+
+
